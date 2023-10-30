b62acbde618c82eb605c61b6110d09c08f2d7da0
karlarojas28 <149344492+karlarojas28@users.noreply.github.com>
2023-10-29 22:45:56 -0500

    Create dbpa.py

---
+++ dbpa.py
@@ -0,0 +1,15 @@
+
+pip install streamlit
+
+pip install plotly
+
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+st.set_page_config(page_title="Datos Aseguradora") # Nombre para configurar la pagina web
+st.header('Datos clientes aseguradora') #Va a ser el titulo de la pagina
+st.subheader('Que informacion se puede obtener de cada clente') #Subtitulo
+
+excel_file = 'fraudes_con_na.xlsx' #Nombre archivo a importar  'xlsx' hace referencia a excel
+
