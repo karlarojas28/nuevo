b4638c91e8e28dd9b1d52fd917d395a3b2fff884
karlarojas28 <149344492+karlarojas28@users.noreply.github.com>
2023-11-14 14:22:03 -0500

    Update dbpa.py

--- dbpa.py
+++ dbpa.py
@@ -9,5 +9,5 @@ st.set_page_config(page_title="Datos Aseguradora") # Nombre para configurar la p
 st.header('Datos clientes aseguradora') #Va a ser el titulo de la pagina
 st.subheader('Que informacion se puede obtener de cada clente') #Subtitulo
 
-excel_file = 'fraudes_con_na.xlsx' #Nombre archivo a importar  'xlsx' hace referencia a excel
+excel_file = 'fraudes_con_na 2.xlsx' #Nombre archivo a importar  'xlsx' hace referencia a excel
 
